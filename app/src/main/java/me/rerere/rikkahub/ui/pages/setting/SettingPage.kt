55c3178496487d6265fbda1b1eaae69b7f85176d
innna327-source <268156079+innna327-source@users.noreply.github.com>
2026-08-02 17:38:34 +0800

    fix: remove global developer mode banner

--- app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingPage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingPage.kt
@@ -48,6 +48,7 @@ import me.rerere.hugeicons.stroke.Brain02
 import me.rerere.hugeicons.stroke.Clapping01
 import me.rerere.hugeicons.stroke.Cloud
 import me.rerere.hugeicons.stroke.Database02
+import me.rerere.hugeicons.stroke.Developer
 import me.rerere.hugeicons.stroke.GlobalSearch
 import me.rerere.hugeicons.stroke.ImageUpload
 import me.rerere.hugeicons.stroke.InLove
@@ -123,6 +124,17 @@ fun SettingPage(vm: SettingVM = koinViewModel()) {
                     BackButton()
                 },
                 scrollBehavior = scrollBehavior,
+                actions = {
+                    if(settings.developerMode) {
+                        IconButton(
+                            onClick = {
+                                navController.navigate(Screen.Developer)
+                            }
+                        ) {
+                            Icon(HugeIcons.Developer, "Developer")
+                        }
+                    }
+                },
                 colors = CustomColors.topBarColors
             )
         },
